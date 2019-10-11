551432c6f5081c021ea35239e09de5cdc456dd53
tilly yannick <yannick13009@gmail.com>
2019-10-12 01:13:07 +0200

    get medicaments get medicament

--- src/main/java/LPY/appliVisiteur/Controller/Visiteur/MedicamentController.java
+++ src/main/java/LPY/appliVisiteur/Controller/Visiteur/MedicamentController.java
@@ -1,12 +1,30 @@
 package LPY.appliVisiteur.Controller.Visiteur;
 
 import LPY.appliVisiteur.Controller.BaseController;
+import LPY.appliVisiteur.Model.Entity.Medicament;
 import LPY.appliVisiteur.Model.Repository.MedicamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class MedicamentController extends BaseController {
     @Autowired
     private MedicamentRepository medicamentRepository;
+
+    @RequestMapping(value = "medicament/{id}", method = RequestMethod.GET)
+    public Optional<Medicament> getDepartement(@PathVariable("id") Long id)
+    {
+        return medicamentRepository.findById(id);
+    }
+
+    @RequestMapping(value = "medicaments", method = RequestMethod.GET)
+    public Iterable<Medicament> getMedicaments()
+    {
+        return medicamentRepository.findAll();
+    }
 }
--- src/main/java/LPY/appliVisiteur/Model/Repository/MedicamentRepository.java
+++ src/main/java/LPY/appliVisiteur/Model/Repository/MedicamentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface MedicamentRepository extends CrudRepository<Medicament, Integer> {
-    Optional<Medicament> findById(Integer id);
+    Optional<Medicament> findById(Long id);
 }
