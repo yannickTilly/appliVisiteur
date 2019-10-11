1eac007bdcf350359f121b3ad146d51a579f6454
tilly yannick <yannick13009@gmail.com>
2019-10-12 01:22:39 +0200

    get periodeTravaillee get periodeTravaillees

--- src/main/java/LPY/appliVisiteur/Controller/Visiteur/PeriodeTravailleeController.java
+++ src/main/java/LPY/appliVisiteur/Controller/Visiteur/PeriodeTravailleeController.java
@@ -1,12 +1,30 @@
 package LPY.appliVisiteur.Controller.Visiteur;
 
 import LPY.appliVisiteur.Controller.BaseController;
+import LPY.appliVisiteur.Model.Entity.PeriodeTravaillee;
 import LPY.appliVisiteur.Model.Repository.PeriodeTravailleeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class PeriodeTravailleeController extends BaseController {
     @Autowired
     private PeriodeTravailleeRepository periodeTravailleeRepository;
+
+    @RequestMapping(value = "periodeTravaillee/{id}", method = RequestMethod.GET)
+    public Optional<PeriodeTravaillee> getDepartement(@PathVariable("id") Long id)
+    {
+        return periodeTravailleeRepository.findById(id);
+    }
+
+    @RequestMapping(value = "periodeTravaillees", method = RequestMethod.GET)
+    public Iterable<PeriodeTravaillee> getPeriodeTravaillees()
+    {
+        return periodeTravailleeRepository.findAll();
+    }
 }
--- src/main/java/LPY/appliVisiteur/Model/Repository/PeriodeTravailleeRepository.java
+++ src/main/java/LPY/appliVisiteur/Model/Repository/PeriodeTravailleeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface PeriodeTravailleeRepository extends CrudRepository<PeriodeTravaillee, Integer> {
-    Optional<PeriodeTravaillee> findById(Integer id);
+    Optional<PeriodeTravaillee> findById(Long id);
 }
