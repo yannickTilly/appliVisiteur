e2d8164434955a0b6f07b54243d4179579b693c1
tilly yannick <yannick13009@gmail.com>
2019-10-12 01:27:40 +0200

    get praticien get praticiens

--- src/main/java/LPY/appliVisiteur/Controller/Visiteur/PraticienController.java
+++ src/main/java/LPY/appliVisiteur/Controller/Visiteur/PraticienController.java
@@ -1,12 +1,30 @@
 package LPY.appliVisiteur.Controller.Visiteur;
 
 import LPY.appliVisiteur.Controller.BaseController;
-import LPY.appliVisiteur.Model.Repository.PeriodeTravailleeRepository;
+import LPY.appliVisiteur.Model.Entity.Praticien;
+import LPY.appliVisiteur.Model.Repository.PraticienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class PraticienController extends BaseController {
     @Autowired
-    private PeriodeTravailleeRepository periodeTravailleeRepository;
+    private PraticienRepository praticienRepository;
+
+    @RequestMapping(value = "praticien/{id}", method = RequestMethod.GET)
+    public Optional<Praticien> getDepartement(@PathVariable("id") Long id)
+    {
+        return praticienRepository.findById(id);
+    }
+
+    @RequestMapping(value = "praticiens", method = RequestMethod.GET)
+    public Iterable<Praticien> getPraticiens()
+    {
+        return praticienRepository.findAll();
+    }
 }
