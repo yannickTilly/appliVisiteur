b74a59ebbfd8991775e74e913ac70c1b654befbe
tilly yannick <yannick13009@gmail.com>
2019-10-13 02:51:10 +0200

    patch user

--- src/main/java/LPY/appliVisiteur/Controller/Visiteur/UserController.java
+++ src/main/java/LPY/appliVisiteur/Controller/Visiteur/UserController.java
@@ -1,12 +1,17 @@
 package LPY.appliVisiteur.Controller.Visiteur;
 
 import LPY.appliVisiteur.Controller.BaseController;
+import LPY.appliVisiteur.Model.Entity.User;
 import LPY.appliVisiteur.Model.Exception.UserNotFoundException;
 import LPY.appliVisiteur.Model.Repository.RapportVisiteRepository;
+import LPY.appliVisiteur.Model.Repository.UserRepository;
+import LPY.appliVisiteur.Model.RequestBody.Visiteur.VisiteurBody;
 import LPY.appliVisiteur.Model.View.Visiteur.UserView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,10 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController extends BaseController
 {
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private RapportVisiteRepository rapportVisiteRepository;
 
-    @RequestMapping("/user")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String getVisiteur() throws UserNotFoundException, JsonProcessingException {
+        return this.createResponse(this.getUser(), UserView.user.class);
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PATCH)
+    public String postVisiteur(@RequestBody VisiteurBody visiteurBody) throws UserNotFoundException, JsonProcessingException {
+
+        User user = this.getUser();
+        if (visiteurBody.getNumeroVoie() != null)
+        {
+            user.setNumeroVoie(visiteurBody.getNumeroVoie());
+        }
+        if (visiteurBody.getCodePostal() != null)
+        {
+            user.setCodePostal(visiteurBody.getCodePostal());
+        }
+        if (visiteurBody.getNomVoie() != null)
+        {
+            user.setNomVoie(visiteurBody.getNomVoie());
+        }
+        if (visiteurBody.getTypeVoie() != null)
+        {
+            user.setTypeVoie(visiteurBody.getTypeVoie());
+        }
+        if (visiteurBody.getVille() != null)
+        {
+            user.setVille(visiteurBody.getVille());
+        }
+        userRepository.save(user);
         return this.createResponse(this.getUser(), UserView.user.class);
     }
 }
--- src/main/java/LPY/appliVisiteur/Model/Entity/User.java
+++ src/main/java/LPY/appliVisiteur/Model/Entity/User.java
@@ -19,21 +19,27 @@ public class User {
     @JsonView(UserView.user.class)
     private String login;
 
+    @JsonView(UserView.user.class)
+    private int numeroVoie;
+
+    @JsonView(UserView.user.class)
+    private String typeVoie;
+
+    @JsonView(UserView.user.class)
+    private String nomVoie;
+
+    @JsonView(UserView.user.class)
+    private String codePostal;
+
+    @JsonView(UserView.user.class)
+    private String ville;
+
     @OneToMany(mappedBy="user")
     private Collection<RapportVisite> rapportVisites ;
 
     @OneToMany(mappedBy = "user")
     @JsonView(UserView.user.class)
     private Collection<PeriodeTravaillee> periodeTravaillees;
-
-    public String getLogin() {
-        return login;
-    }
-
-    public User setLogin(String login) {
-        this.login = login;
-        return this;
-    }
 
     public long getId() {
         return id;
@@ -50,6 +56,60 @@ public class User {
 
     public User setNom(String nom) {
         this.nom = nom;
+        return this;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public User setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public int getNumeroVoie() {
+        return numeroVoie;
+    }
+
+    public User setNumeroVoie(int numeroVoie) {
+        this.numeroVoie = numeroVoie;
+        return this;
+    }
+
+    public String getTypeVoie() {
+        return typeVoie;
+    }
+
+    public User setTypeVoie(String typeVoie) {
+        this.typeVoie = typeVoie;
+        return this;
+    }
+
+    public String getNomVoie() {
+        return nomVoie;
+    }
+
+    public User setNomVoie(String nomVoie) {
+        this.nomVoie = nomVoie;
+        return this;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public User setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+        return this;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public User setVille(String ville) {
+        this.ville = ville;
         return this;
     }
 
