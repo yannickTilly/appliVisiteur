7c7cc5ba8e632ea9de23e6baf76e27bd2223fba1
tilly yannick <yannick13009@gmail.com>
2019-11-18 22:54:03 +0100

    code gen

--- src/main/java/LPY/appliVisiteur/CodeGen/Model/ApliCliModel.java
+++ src/main/java/LPY/appliVisiteur/CodeGen/Model/ApliCliModel.java
@@ -1,5 +1,57 @@
 package LPY.appliVisiteur.CodeGen.Model;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApliCliModel {
-    
+    private List<File> files;
+    public static void main(String[] args) {
+        ApliCliModel apliCliModel = new ApliCliModel();
+        apliCliModel.addFile(new File("./src/main/java/LPY/appliVisiteur/Controller/Visiteur/VisitorDepartmentController.java"));
+        apliCliModel.generate();
+    }
+
+    public ApliCliModel() {
+        this.files = new ArrayList<>();
+    }
+
+    public void addFile(File file)
+    {
+        files.add(file);
+    }
+
+    public void generate()
+    {
+        for(File file : files)
+        {
+            try {
+                CompilationUnit compilationUnit = StaticJavaParser.parse(file);
+                List<ClassOrInterfaceDeclaration> rest = getRestController(compilationUnit);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<ClassOrInterfaceDeclaration> getRestController(CompilationUnit compilationUnit)
+    {
+        List<MarkerAnnotationExpr> markerAnnotationExprs = compilationUnit.findAll(MarkerAnnotationExpr.class);
+        List<ClassOrInterfaceDeclaration> restControllers = new ArrayList<ClassOrInterfaceDeclaration>();
+        for (MarkerAnnotationExpr markerAnnotationExpr : markerAnnotationExprs)
+        {
+            if (markerAnnotationExpr.getName().getIdentifier().equals("RestController"))
+            {
+                ClassOrInterfaceDeclaration restController;
+                markerAnnotationExpr.findAncestor(ClassOrInterfaceDeclaration.class).ifPresent(restControllers::add);
+            }
+        }
+        return restControllers;
+    }
 }
