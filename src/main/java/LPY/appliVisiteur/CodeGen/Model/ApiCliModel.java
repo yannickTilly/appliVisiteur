ee0760c14e1138ed8f13efda7055b11145138a0f
yannick <yannick13009@gmail.com>
2019-11-25 14:14:29 +0100

    commit pour paul

---
+++ src/main/java/LPY/appliVisiteur/CodeGen/Model/ApiCliModel.java
@@ -0,0 +1,26 @@
+package LPY.appliVisiteur.CodeGen.Model;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiCliModel {
+    private List<RouteModel> routeModels;
+
+    public ApiCliModel addRouteModel(RouteModel routeModel) {
+        routeModels.add(routeModel);
+        return this;
+    }
+}
